ad6f6757ec694b290a22256f473dc8e6f8a7e9de
Mairon Azevedo <mairondepaiva@gmail.com>
2023-11-16 17:34:24 -0300

    Melhorias no template e aumentando a quantidade de informações proporcionadas

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import csv
 import os
 
 class Lecture:
-    def __init__(self, name, duration, date):
+    def __init__(self, activity, name, duration, date):
+        self.activity = activity
         self.name = name
         self.duration = duration
         self.date = date
@@ -15,8 +16,9 @@ class Lecture:
         return f'Informações da palestra {self.name}:\nNome: {self.name}\nDuração: {self.duration}\nData: {self.date}'
     
 class Participant:
-    def __init__(self, name, registration, email, category, hoursGranted):
+    def __init__(self, name, course, registration, email, category, hoursGranted):
         self.name = name
+        self.course = course
         self.registration = registration
         self.email = email
         self.category = category
@@ -38,20 +40,22 @@ with open('attendance.csv', mode='r', encoding="UTF-8") as file:
     csvFile = csv.reader(file)
 
     for line in csvFile:
-        lecture = Lecture(line[0], "2h", "19/05/2023")
-        participant = Participant(line[1].title().strip(), line[2], line[3], line[4], line[5])
+        lecture = Lecture("Palestra", line[0], "2h", "19/05/2023")
+        participant = Participant(line[1].title().strip(), line[2], line[3], line[4], line[5], line[6])
         time = datetime.datetime.now()
 
         context = {
             "name": participant.name,
+            "course": participant.course,
             "registration": participant.registration,
+            "activity": lecture.activity,
             "lectureName": lecture.name,
             "category": participant.category,
             "hoursGranted": participant.hoursGranted,
             "now": time
         }
 
-        documentName = f'./certificados/certificado-de-participacao_{participant.name.replace(" ", "-")}_{lecture.name}'
+        documentName = f'./certificados/certificado-de-participacao_{participant.name.replace(" ", "-")}_{lecture.name.replace(" ", "-")}'
         document.render(context)
         document.save(f'{documentName}.docx')
 
